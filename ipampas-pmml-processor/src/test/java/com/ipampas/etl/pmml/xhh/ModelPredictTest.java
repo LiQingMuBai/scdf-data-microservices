ac454d324bab9c3e486b40da62bea1323da7d201
Young <cywhoyi@gmail.com>
2018-01-03 21:35:54 +0800

    support pmml model

--- ipampas-pmml-processor/src/test/java/com/ipampas/etl/pmml/xhh/ModelPredictTest.java
+++ ipampas-pmml-processor/src/test/java/com/ipampas/etl/pmml/xhh/ModelPredictTest.java
@@ -10,11 +10,11 @@ public class ModelPredictTest {
 
     public static void main(String[] args) {
         Map<String, Object> inputData = new HashMap<String, Object>();
-        inputData.put("aa", 10.0);
-        inputData.put("bb", 10.0);
-        inputData.put("cc", 10.0);
-        inputData.put("dd", 10.0);
-        inputData.put("ee", 10.0);
+        inputData.put("aa", 1.0);
+        inputData.put("bb", 1.0);
+        inputData.put("cc", 1.0);
+        inputData.put("dd", 1.0);
+        inputData.put("ee", 1.0);
 
         Map<String, Double> resultMap = ModelPredict.predict(inputData);
         double result = resultMap.get("label");
